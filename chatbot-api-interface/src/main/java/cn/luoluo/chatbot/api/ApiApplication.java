9d8b07a08ee3bbb180b4f44eb5ff1161654a13cd
罗继文 <luojiwen@jkstack.com>
2023-02-18 21:48:49 +0800

    通过cookie获取接口信息

--- chatbot-api-interface/src/main/java/cn/luoluo/chatbot/api/ApiApplication.java
+++ chatbot-api-interface/src/main/java/cn/luoluo/chatbot/api/ApiApplication.java
@@ -3,6 +3,8 @@ package cn.luoluo.chatbot.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
---
+++ chatbot-api-interface/src/test/java/cn/luoluo/chat/api/test/ScannerApiTest.java
@@ -0,0 +1,26 @@
+package cn.luoluo.chat.api.test;
+
+
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class ScannerApiTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("你想对服务器说什么 ");
+        Scanner input = new Scanner(System.in);
+        String next = input.nextLine();
+        System.out.println(next);
+    }
+
+
+    @Test
+    public void test() {
+        System.out.println("你想对服务器说什么 ");
+        Scanner input = new Scanner(System.in);
+        String next = input.nextLine();
+        System.out.println(next);
+    }
+}
